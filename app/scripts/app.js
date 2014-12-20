234a2df3f04876e24b19aaa0f4103d8c1d88cfc4
Ladex <ladipoilori@yahoo.com>
2014-12-20 12:46:07 +0000

    corrected resubmiting navigation header

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,7 +18,8 @@ angular
     'ngTouch',
     'mgcrea.ngStrap'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider,$locationProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
